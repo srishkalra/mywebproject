b774e44cd92386605744ddb726e60149ddeeb4bf
srishkalra <69515294+srishkalra@users.noreply.github.com>
2020-08-18 12:16:29 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,9 +1,15 @@
+######Author:Srishti Kalra:##########################################################################
+##############A simple Webapplication based on flask which has 2 web pages as Home and Info page##########
+################Logging is enabled for this APP as INFO level##############################
+##################Logs file will be saved in "myapp.log"###################################################
+
 # Importing flask module in the project
 # An object of Flask class is our WSGI application
 from flask import Flask,jsonify
 import logging
 import git
 import os
+#Defining OS ENV variable passed from DocKer file
 port = os.environ.get('PORT')
 version = os.environ.get('VERSION')
 debug = os.environ.get('DEBUG')
@@ -11,12 +17,13 @@ debug = os.environ.get('DEBUG')
 app = Flask(__name__)
 #Initializing object of logging.logger
 applog = logging.getLogger(__name__)
+#Capturing the git information from parent directory where it is cloned
 git_repo = git.Repo(search_parent_directories=True)
+#Evaluating the SHA id.
 sha_id = git_repo.head.object.hexsha
 #Setting the log level as INFO
 applog.setLevel(logging.INFO)
-
-#print(applog.getEffectiveLevel())
+#Using the basicCOnfig function to format and redirect the logs  to file
 logging.basicConfig(filename="myapp.log",format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 service = [
     {
@@ -29,14 +36,15 @@ service = [
         'loglevel': f"{logging.getLevelName(applog.level)}"
     }
 ]
+#Defining Home Page 
 @app.route('/',methods=['GET'])
 def home():
     app.logger.info('Displaying hello')
     return 'Welcome to Web APP!:'
-
+#Definig /info page
 @app.route('/info', methods=['GET'])
 def get_myapp_info():
     app.logger.info('Displaying the info logs')
-    return jsonify({'service': service},200)
-
+    return jsonify({'service': service},200) #Returning Json object
+#Running the app with Port Number,debug information defined from OS Environment and Host Ip is set to run any Host where this app is deployed)
 app.run(host='0.0.0.0', port="{}".format(port),debug="{}".format(debug))
